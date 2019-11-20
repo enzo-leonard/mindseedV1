b02ba3369ed741d48b1ef7f2aa3628b1a1979cab
Melvin <melvin.vmegen@gmail.com>
2019-11-20 13:12:51 +0100

    Dictionary-done

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,5 +1,6 @@
 import "bootstrap";
 import initPhotosUpload from "../components/initPhotosUpload.js"
-
+import initDictionary from "../components/initDictionary.js"
 
 initPhotosUpload();
+initDictionary();
