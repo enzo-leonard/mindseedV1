1321f7566793a2df49bbaaf1f86aa77532d26d34
Melvin <melvin.vmegen@gmail.com>
2019-11-27 12:05:49 +0100

    almost done

---
+++ app/helpers/meta_tags_helper.rb
@@ -0,0 +1,15 @@
+module MetaTagsHelper
+  def meta_title
+    content_for?(:meta_title) ? content_for(:meta_title) : DEFAULT_META["Mindseed - growing to your full potential"]
+  end
+
+  def meta_description
+    content_for?(:meta_description) ? content_for(:meta_description) : DEFAULT_META["Mindseed is the perfect helper for people willing to learn on the long term"]
+  end
+
+  def meta_image
+    meta_image = (content_for?(:meta_image) ? content_for(:meta_image) : DEFAULT_META["favicon_mindseed.png"])
+    # little twist to make it work equally with an asset or a url
+    # meta_image.starts_with?("http") ? meta_image : image_url(meta_image)
+  end
+end
